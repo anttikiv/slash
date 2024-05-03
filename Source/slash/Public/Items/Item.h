997347dd3364414a9b86ad197e31299910877e89
Anttikiv <anttikiv.dev@gmail.com>
2024-05-03 17:26:27 +0300

    S5.39

--- Source/slash/Public/Items/Item.h
+++ Source/slash/Public/Items/Item.h
@@ -14,13 +14,12 @@ class SLASH_API AItem : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AItem();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 };
