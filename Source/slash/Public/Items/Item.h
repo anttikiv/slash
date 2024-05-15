14ec594b5f3fc5317aa5d6fea941a96d3063c5bb
Anttikiv <anttikiv.dev@gmail.com>
2024-05-15 21:10:28 +0300

    Section 11: IK Righ, Retargetting, Overlap->attach to socket

--- Source/slash/Public/Items/Item.h
+++ Source/slash/Public/Items/Item.h
@@ -43,14 +43,13 @@ protected:
 
 	UFUNCTION()
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
-
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* ItemMesh;
 private:
 	
-	UPROPERTY(VisibleInstanceOnly)
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime;
-	
-	UPROPERTY(VisibleAnywhere)
-	UStaticMeshComponent* ItemMesh;
 
 	UPROPERTY(VisibleAnywhere)
 	USphereComponent* Sphere; 
